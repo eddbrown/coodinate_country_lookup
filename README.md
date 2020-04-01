d9c95ef5e4dbdc8a04788fe6fa53508c5fe85a81
Edward Brown <edward_brown@system76-pc.localdomain>
2020-04-01 10:31:02 +0100

    Second commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+This package contains a method to return a country given the latitude and longitude of a point. Crucially it does this offline without having to use an online service like google.
--- setup.py
+++ setup.py
@@ -7,21 +7,11 @@ setup(
   description = 'Returns the country of the latitude longitude tuple offline',   # Give a short description about your library
   author = 'Edward Brown',                   # Type in your name
   author_email = 'edward.j.e.brown@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  url = 'https://github.com/eddbrown/coodinate_country_lookup',   # Provide either the link to your github or to your website
   keywords = ['COORIDNATES', 'COUNTRY', 'LOOKUP'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'shapely',
           'geopandas',
       ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+  zip_safe=False
 )
